9352ab15239d8c6fd93d2fd8dcee58e407cc7a7b
Alexis <alexis@bourgoin.eu>
2018-10-28 21:09:27 +0100

    modified taxoFetch and addTaxo, now adds worms ID for every rank, test before use pls

--- src/data_structuration/addTaxonomy.py
+++ src/data_structuration/addTaxonomy.py
@@ -3,6 +3,7 @@ import config
 import pandas as pd
 import numpy as np
 import urllib
+import json
 
 def fetchID(name):
     try:
@@ -29,19 +30,22 @@ def fetchTaxonomy(ID):
     x = data
     while x['child'] is not None: 
         x = x['child']
-        taxo[x['rank'].lower()] = x['scientificname']
+        taxo[x['rank'].lower()] = {"name":x['scientificname'], "id":x["AphiaID"]}
     return taxo
-
-def addTaxonomy(df_target, df_source):
+    
+    
+def addTaxonomy(df_target, df_source, **kwargs):
     '''
     Be sure to have a species column in both arrays before continuing
     Be sure to put the taxonomy values in df_source
     '''
     df1 = df_target
     df2 = df_source
-    ranks = ['worms_id', 'kingdom', 'phylum', 'class', 'order', 'family', 'genus', 'species']
-    for rank in ranks:
-        df1[rank] = None
+    ranks = ['kingdom', 'phylum', 'class', 'order', 'family', 'genus', 'species']
+    columns += ranks + [ rank + "_id" for rank in ranks]
+    
+    for c in columns:
+        df1[c] = None
     try:
         df1_sp = set(df1.species):
         df2_sp = set(df2.species):
@@ -51,10 +55,10 @@ def addTaxonomy(df_target, df_source):
     
     for sp in df1_sp:
         if sp in df2_sp: #if target species among source species, add taxonomy
-            for rank in ranks:
-                rank_set = set(df2[df2.species == sp][rank])
-                if len(rank_set) == 1:
-                    df1.at[df1.species == sp, rank] = rank_set.pop()
+            for c in columns:
+                c_set = set(df2[df2.species == sp][c])
+                if len(c_set) == 1:
+                    df1.at[df1.species == sp, c] = c_set.pop()
                 else:
                     print("Please sanitize data first")
                     pass
@@ -62,7 +66,8 @@ def addTaxonomy(df_target, df_source):
         else:
             taxonomy = fetchTaxonomy(fetchID(sp))
             for rank in ranks:
-                df1.at[df1.species == sp, rank] = taxonomy[rank]
+                df1.at[df1.species == sp, rank] = taxonomy[rank]["name"]
+                df1.at[df1.species == sp, rank+"_id"] = taxonomy[rank]["id"]
     return df1
     
             
