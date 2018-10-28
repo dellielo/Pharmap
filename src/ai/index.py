2db3a47532cb8f7c54ecfafbb94c3e7c0116687f
Noe <noe.rivals@epitech.eu>
2018-10-28 16:15:35 +0100

    [+](src/ai): worked on ai particulary the preparation of the data sent to keras

--- src/ai/index.py
+++ src/ai/index.py
@@ -4,20 +4,49 @@ import sys
 sys.path.insert(0, './src/data_structuration/')
 import tools
 import conf
+import neuralNetwork
+from sklearn.model_selection import train_test_split
+from keras_pandas.Automater import Automater
 
-def selectTrainData(mainTab):
-    print(mainTab.columns)
-    selectedFields = []
+def orderColumns(tab):
+    cols = tab.columns.tolist()
     for field in conf.inputFileds:
-        if field in mainTab.columns:
-            selectedFields.append(field)
-        else:
-            print("Can t find column ", field, " in mainTab")
-    newTab =  mainTab.loc[:,selectedFields]
-    return newTab
+        if not field in cols:
+            raise ("Can t find column ", field, " in tab")
+    cols = conf.inputFileds + [x for x in cols if x not in conf.inputFileds]
+    return tab[cols]
+
+def cleanTab(tab):
+    return tab.dropna(subset=conf.inputFileds)
+
+def addOutputColumn(tab):
+    print('adding output column')
+    minSampleSize = 1000
+    tab['counts'] = tab.groupby('ScientificName')['ScientificName'].transform('count') #add coulumn counts
+    tab = tab[tab.counts > minSampleSize] # select all element that have at least $minSampleSize element
+    print('have', len(tab.groupby('counts').size()), 'type with more than', minSampleSize, 'sample')
+    tab = tab.assign(output=(tab['ScientificName']).astype('category').cat.codes) #add unique id to each scientific name
+    return tab
+
+def getInputOutput(tab):
+
+    x = tab.loc[:,conf.inputFileds].values
+    y = tab.loc[:,conf.outputField].values
+
+    return (x, y)
 
 if __name__=="__main__":
     data = tools.load('./data/out/')
     for key in data:
-        print(data[key])
-    #selectTrainData(mainTable)
+        tab = data[key]
+        tab = orderColumns(tab)
+        tab = cleanTab(tab)
+        tab = addOutputColumn(tab)
+        x, y = getInputOutput(tab)
+        print(x[0], y[0])
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1)
+        print(x_train[0], y_train[0])
+        outputNb = tab[conf.outputField].max() + 1 # +1 beacuase index start at 0
+        nn = neuralNetwork.NeuralNetwork(outputNb)
+        nn.train(x_train, y_train)
+        nn.evaluate(x_test, y_test)
--- src/ai/neuralNetwork.py
+++ src/ai/neuralNetwork.py
@@ -4,18 +4,20 @@ import conf
 
 class NeuralNetwork:
 
-    def __init__(self):
-        self.createNn()
+    def __init__(self, outputNb):
+        self.createNn(outputNb)
 
-    def createNn(self):
+    def createNn(self, outputNb):
         self.model = keras.Sequential([
             keras.layers.Flatten(input_shape=(len(conf.inputFileds),)),
-            keras.layers.Dense(4, activation=tf.nn.relu),
-            keras.layers.Dense(2, activation=tf.nn.softmax)
+            keras.layers.Dense(100, activation=tf.nn.relu),
+            keras.layers.Dense(100, activation=tf.nn.relu),
+            keras.layers.Dense(100, activation=tf.nn.relu),
+            keras.layers.Dense(outputNb, activation=tf.nn.softmax)
         ])
 
         self.model.compile(optimizer=tf.train.AdamOptimizer(),
-                      loss='categorical_crossentropy',
+                      loss='sparse_categorical_crossentropy',
                       metrics=['accuracy'])
 
     def evaluate(self, testInput, testOutput):
@@ -26,6 +28,10 @@ class NeuralNetwork:
     def predict(self, inputsToPredict): #input is an array
         pred = self.model.predict(inputsToPredict)
         return pred
+
+    def test(self, testInput, testOutput):
+        test_loss, test_acc = self.model.evaluate(testInput, testOutput)
+        print('Test accuracy:', test_acc, "loss: ", test_loss)
 
     def train(self, trainInput, trainOutput):
         self.model.fit(trainInput, trainOutput, steps_per_epoch=5000)
