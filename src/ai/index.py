561ae8c58c7046f3bf5e74a6b454177549e0cd54
dellielo <dellielo03@gmail.com>
2018-10-29 19:00:32 +0100

    Add SMOTE for over-sampling + test  other network

--- src/ai/index.py
+++ src/ai/index.py
@@ -3,6 +3,9 @@
 import sys
 sys.path.insert(0, './src/data_structuration/')
 import tools
+# https://imbalanced-learn.readthedocs.io/en/stable/over_sampling.html
+from imblearn.over_sampling import SMOTE, ADASYN, BorderlineSMOTE
+import numpy as np
 import conf
 import neuralNetwork
 from sklearn.model_selection import train_test_split
@@ -36,6 +39,11 @@ def getInputOutput(tab):
     y = tab.loc[:,conf.outputField].values
     return (x, y)
 
+def makeOverSamples(X,y):
+    sm = SMOTE()
+    X, y = sm.fit_resample(X, y)
+    return X,y
+
 if __name__=="__main__":
     data = tools.load('./data/out/')
     for key in data:
@@ -46,15 +54,38 @@ if __name__=="__main__":
         x, y = getInputOutput(tab)
         x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=42, test_size=0.25)
         
+        
         # Standardization 
         scaler = preprocessing.StandardScaler().fit(x_train)
         x_train = scaler.transform(x_train)
         x_test = scaler.transform(x_test)
 
+        x_train, y_train = makeOverSamples(x_train, y_train)
+
         outputNb = len(tab[conf.outputField].unique()) 
+        labels = sorted(tab['ScientificName'].unique())
+
         nn = neuralNetwork.NeuralNetwork(outputNb)
         nn.train(x_train, y_train, epochs=100)
         nn.evaluate(x_test, y_test)
+
         y_pred = nn.predict_classes(x_test)
-        labels = sorted(tab['ScientificName'].unique())
+        prob = nn.predict(x_test)
+
+        idx2label = dict((v,k) for k,v in zip(labels, range(0,outputNb)))
+        print(idx2label)
+        errors = np.where(y_pred != y_test)[0]
+        print("No of errors = {}/{}".format(len(errors),len(y_test)))
+
+        for i in range(len(errors[:5])):
+            pred_class = np.argmax(prob[errors[i]])
+            print(pred_class)
+            pred_label = idx2label[pred_class]
+            true_label = idx2label[y_test[errors[i]]]
+
+            print('Original label: [{}], Prediction :[{}], confidence : {:.3f}'.format(
+                true_label,
+                pred_label,
+                prob[errors[i]][pred_class]))
+            
         print (metrics.classification_report(y_test, y_pred, target_names=labels))
