d5f3d9ec7ca758df2cd96de3a488082a062026a9
Alexis <alexis@bourgoin.eu>
2018-11-05 14:11:56 +0100

    added DRUGCLASS dictionnary to link molecule and pathology

--- src/data_structuration/molregex.py
+++ src/data_structuration/molregex.py
@@ -36,7 +36,40 @@ orin|\
 ine|\
 ocin)"
 
-
 REG = re.compile("[a-zA-Z0-9]+{chem}+(( |-)[a-zA-Z0-9--]*{chem}(s)*)*(( |-)[A-Z]?[0-9]*)* ".format(chem = CHEMS))
 
+DRUGCLASS ={
+    'anticancer':["anti-tumour", 
+                  "anti-tumor", 
+                  "cytotoxicity", 
+                  "cytotoxic", 
+                  "growth inhibitor",
+                  "growth inhibiting"
+                  "anti-cancer", 
+                  "antineoplastic"],
+    'antioxydant':["anti-oxydant",
+                   "antioxydative"],
+    'antibiotic':["bacteriostatic", 
+                  "anti-microbial", 
+                  "antibiotic", 
+                  "bactericide", 
+                  "S. aureus"],
+    'antiparasitic':["Plasmodium falciparum,
+                     "antimalarial",
+                     "antiparasitic"
+                     "antiprotozoal",
+                     "antihelminthic",
+                     "anticestode",
+                     "antinematode",
+                     "antiamobeic",
+                     "antitrematode",
+                     "amoebicid"],
+    'antifungal':["antifungic",
+                  "antifungal"],
+    'antiviral':["antiviral"],
+    'anti-inflammatory':["anti-inflammatory"],
+    'anesthetic/analgesic':["anesthetic", 
+                            "pain inhibitor",
+                            "analgesic"]    
+}
 
