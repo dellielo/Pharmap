1a1f97a840c982f67780116bc2017f7c28a3ab97
Alexis <alexis@bourgoin.eu>
2018-12-07 19:47:30 +0100

    Added filter less than 1e-6 probability

--- src/data_structuration/predict_to_csv.py
+++ src/data_structuration/predict_to_csv.py
@@ -2,6 +2,7 @@ import json
 import os
 import pprint
 import sys
+import argparse
 import numpy as np
 import pandas as pd
 
@@ -10,16 +11,23 @@ def json_to_array(file_path):
         lines = f.readlines()
         for line in lines:
             v = json.loads(line)
-	    if "array" not in locals():
+            if "array" not in locals(): #if first line
                 array = np.array(list(v.values()))
+                print(sum(list(v.values())[0]))
             else:
-                array = np.vstack( ( array, np.array(list(v.values())) ))
+                print(sum(list(v.values())[0]))
+                array = np.vstack( ( array, np.array(list(v.values())) )) #else lets add this new row
+    
+    #We remove near zero values, to quickly check if sp is present or not and reduce number of relationship
+    nz_filter = np.vectorize(lambda x: x if x<1E-6 else x)
+    array = nz_filter(array)
+    
     return array
 
 def nn_class_to_columns(file_path):
     with open(file_path) as f:
        columns = []
-       lines = f.read().splitlines()
+       lines = f.read().splitlines() #to avoid \n
        for line in lines:
            columns.append(line)
     return columns
@@ -30,5 +38,16 @@ def predict_to_csv(predict_path, class_path, out_path):
     df = pd.DataFrame(array, columns=col)
     df.to_csv(out_path)
 
+def main():
+    parser = argparse.ArgumentParser(description='make google ML engine output readable for neo4j and/or raster integration')
+    parser.add_argument('--path_predict', default="predict")
+    parser.add_argument('--path_class', default="classes.txt")
+    parser.add_argument('--output', default="default_out")
+    
+    args = parser.parse_args()
+    
+    predict_to_csv(args.path_predict, args.path_class, args.output)
+
 if __name__=="__main__":
-    predict_to_csv("/home/idiot_cr0w/micro.json", "/home/idiot_cr0w/Pharmap/data/bestModel/model3/classes.txt", "test.csv")
+    main()  
+    #predict_to_csv("/home/idiot_cr0w/micro.json", "/home/idiot_cr0w/Pharmap/data/bestModel/model3/classes.txt", "test.csv")
